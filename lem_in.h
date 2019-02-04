8f04a109a1248843982057dadc02c3306cf9e1ac
Matthew LEROY <matleroy@e1r6p5.42.fr>
2019-02-04 21:09:52 +0100

    begin parse

--- lem_in.h
+++ lem_in.h
@@ -2,5 +2,12 @@
 # define LEM_IN_H
 #include "libft.h"
 
+typedef struct	s_room
+{
+	int			x;
+	int			y;
+	char		*name;
+}				t_room;
+
 int		parse_infos();
 #endif
--- parsing.c
+++ parsing.c
@@ -6,20 +6,46 @@
 /*   By: matleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 18:00:33 by matleroy          #+#    #+#             */
-/*   Updated: 2019/02/04 18:38:13 by matleroy         ###   ########.fr       */
+/*   Updated: 2019/02/04 21:08:25 by matleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include <fcntl.h>
 
+
+int	get_start(char *line)
+{
+	t_room start;
+
+	(void)start;
+	if (!strcmp("##start", line))
+	{	
+		ft_printf("{yellow} good\n");
+		if (get_next_line(0, &line) == 1)
+		{
+			start.name = ft_strcdup(line, ' ');	
+		}
+		ft_printf("{green} name = %s\n", start.name);
+	}
+	return (0);
+}
+
 int	parse_infos()
 {
 	char *line;
+	int		ant;
 
+	ant = -1;
 	while (get_next_line(0, &line) == 1)
 	{
-		ft_printf("%s\n", line);
+		if (ant < 0)
+		{
+			ant = ft_atoi(line);
+		}
+		ft_printf("{red}%d\n", ant);
+		ft_printf("{cyan}%s\n", line);
+		get_start(line);
 	}
 	return (0);
 }
