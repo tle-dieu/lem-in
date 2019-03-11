2d60a4e529479f5770dd514fcf5e868fb30f9858
Thomas LE DIEU DE VILLE <tle-dieu@e3r8p8.42.fr>
2019-03-11 18:26:30 +0100

    improved print

--- ants_repart.c
+++ ants_repart.c
@@ -6,7 +6,7 @@
 /*   By: tle-dieu <tle-dieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 16:29:00 by tle-dieu          #+#    #+#             */
-/*   Updated: 2019/03/11 18:23:12 by tle-dieu         ###   ########.fr       */
+/*   Updated: 2019/03/11 18:35:33 by tle-dieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,21 @@ int		main(int ac, char **av)
 	{
 		i = 1;
 		ant_max = ft_atoi(av[i++]);
-		ft_printf("nb fourmis: %d\n", ant_max);
+		ft_printf("{rgb(12,231,58)}nb fourmis: {reset}%d\n", ant_max);
 		nb_path = ac - 2;
-		ft_printf("nb path: %d\n", nb_path);
+		ft_printf("{rgb(12,231,58)}nb path: {reset}%d\n", nb_path);
 		if (!(path = (int *)malloc(sizeof(int) * nb_path)))
 			return (1);
+		ft_printf("{rgb(12,231,58)}lpath: {reset}");
 		while (i < ac)
 		{
 			path[j] = ft_atoi(av[i]);
-			ft_printf("chemin: %d\n", path[j]);
+			ft_printf("%d ", path[j]);
 			lmax += path[j];
 			i++;
 			j++;
 		}
-		ft_printf("lmax: %d\n", lmax);
+		ft_printf("\n{rgb(12,231,58)}lmax: {reset}%d\n", lmax);
 		i = 0;
 		while (i < j)
 		{
@@ -58,9 +59,10 @@ int		main(int ac, char **av)
 			ant_max -= ant;
 			k += ant;
 			nb_path--;
-			ft_printf("chemin %d: %d reste: %d\n", i++, ant, ant_max);
+			ft_printf("{rgb(0,188,218)}chemin %d: {reset}%d {rgb(0,188,218)}reste: {reset}%d\n", i++, ant, ant_max);
 		}
-		ft_printf("ant: %d\n", k);
+		ft_printf("{rgb(251,196,15)}ants total: {reset}%d\n", k);
+		ft_printf("{rgb(251,196,15)}nb instructions: {reset}%d\n", ant + path[i - 1]);
 	}
 	else
 		ft_printf("usage: ./ant_test [ants] [lpath lpath lpath...]\n");
