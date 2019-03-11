6e103d11f3fe8ea026db4f9b4d8738ab0a3bd6bf
Thomas LE DIEU DE VILLE <tle-dieu@e3r8p8.42.fr>
2019-03-11 18:15:46 +0100

    ants repart corrige

--- ants_repart.c
+++ ants_repart.c
@@ -6,11 +6,11 @@
 /*   By: tle-dieu <tle-dieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 16:29:00 by tle-dieu          #+#    #+#             */
-/*   Updated: 2019/03/11 16:52:23 by tle-dieu         ###   ########.fr       */
+/*   Updated: 2019/03/11 18:15:32 by tle-dieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/includes/libft.h"
+#include "libft.h"
 #include <stdlib.h>
 
 int		main(int ac, char **av)
@@ -22,19 +22,21 @@ int		main(int ac, char **av)
 	int lmax;
 	int ant_max;
 	int ant;
+	int k;
 
 	lmax = 0;
+	k = 0;
 	j = 0;
-	if (ac >= 4)
+	if (ac >= 3)
 	{
 		i = 1;
 		ant_max = ft_atoi(av[i++]);
 		ft_printf("nb fourmis: %d\n", ant_max);
-		nb_path = ft_atoi(av[i++]);
+		nb_path = ac - 2;
 		ft_printf("nb path: %d\n", nb_path);
-		if (!(path = (int *)malloc(sizeof(int))))
+		if (!(path = (int *)malloc(sizeof(int) * nb_path)))
 			return (1);
-		while (j < nb_path && i < ac)
+		while (i < ac)
 		{
 			path[j] = ft_atoi(av[i]);
 			ft_printf("chemin: %d\n", path[j]);
@@ -49,9 +51,16 @@ int		main(int ac, char **av)
 			ant = (ant_max + lmax) / nb_path - path[i];
 			if (ant < 0)
 				ant = 0;
-			ft_printf("chemin %d: %d\n", ++i, ant);
+			lmax -= path[i];
+			ant_max -= ant;
+			k += ant;
+			nb_path--;
+			ft_printf("chemin %d: %d reste: %d\n", i++, ant, ant_max);
 		}
+		ft_printf("ant: %d\n", k);
 	}
 	else
-		ft_printf("./a.out [nb_fourmis] [nb_way] lway lway lway...\n");
+		ft_printf("usage: ./ant_test [ants] [lpath lpath lpath...]\n");
 }
+
+/* gcc -o ant_test ants_repart.c -I libft/includes/ -L./libft -lft*/
