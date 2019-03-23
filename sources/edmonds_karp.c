0ec383905937ba10a507d8e211647d8df6cd3fc5
Thomas LE DIEU DE VILLE <tle-dieu@e1r1p11.42.fr>
2019-03-23 21:26:17 +0100

    ajout de nouvellees map, solve.c mv en edmonds_karp.c

---
+++ sources/edmonds_karp.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   edmonds_karp.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-dieu <tle-dieu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/23 21:18:36 by tle-dieu          #+#    #+#             */
+/*   Updated: 2019/03/23 21:21:11 by tle-dieu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+#include <stdlib.h>
+
+int		augmenting_path(t_lemin *l, char **flow)
+{
+	t_room *room;
+
+	room = l->end;
+	if (!room->prev)
+		return (0);
+	while (room->prev)
+	{
+		flow[room->id][room->prev->id]--;
+		flow[room->prev->id][room->id]++;
+		room = room->prev;
+	}
+	return (1);
+}
+
+static	char **init_flow(t_lemin *l)
+{
+	char	**flow;
+	int		i;
+	int		j;
+
+	j = 0;
+	if (!(flow = (char **)malloc(sizeof(char *) * l->nb_room)))
+		return (NULL);
+	while (j < l->nb_room)
+	{
+		if (!(flow[j] = (char *)malloc(sizeof(char) * l->nb_room)))
+		{
+			free_tab((void **)flow, j);
+			return (NULL);
+		}
+		i = 0;
+		while (i < l->nb_room)
+			flow[j][i++] = 0;
+		j++;
+	}
+	return (flow);
+}
+
+long	get_steps(t_lemin *l, int max_flow, int tlen)
+{
+	int len;
+	int ant_total;
+	int ant;
+	int i;
+	int j;
+
+	j = 0;
+	i = 0;
+	ant_total = l->ant;
+	while (j < l->start->nb_links)
+	{
+		if (l->start->links[j]->flow)
+		{
+			len = l->start->links[j]->flow;
+			if ((ant = ((long)ant_total + tlen) / (max_flow - i) - len) <= 0)
+				return (-1);
+			tlen -= len;
+			ant_total -= ant;
+			i++;
+		}
+		j++;
+	}
+	if (l->steps == -1 || (long)ant + len < l->steps)
+		return ((long)ant + len);
+	return (0);
+}
+
+int		edmonds_karp(t_lemin *l)
+{
+	char	**flow;
+	int		max_flow;
+
+	max_flow = 0;
+	if (!(flow = init_flow(l)))
+		return (0);
+	while (bfs(l, flow))
+	{
+		print_paths(l, flow); //a retirer
+		max_flow++;
+		if (!(new_graph(l, flow, max_flow)))
+			break ;
+	}
+	free_tab((void **)flow, l->nb_room);
+	return (max_flow);
+}
